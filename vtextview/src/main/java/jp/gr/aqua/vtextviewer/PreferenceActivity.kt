a36cce9442315ad3428c5af6be986c9bcd7ada04
Jiro Mori <pandora@aquamarine.sakura.ne.jp>
2017-07-23 12:40:10 +0900

    ライブラリとして動作させるために調整

--- vtextview/src/main/java/jp/gr/aqua/vtextviewer/PreferenceActivity.kt
+++ vtextview/src/main/java/jp/gr/aqua/vtextviewer/PreferenceActivity.kt
@@ -9,5 +9,6 @@ class PreferenceActivity : AppCompatActivity(), PreferenceFragment.OnFragmentInt
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vtext_preference)
+        setTitle(R.string.vtext_app_name)
     }
 }
